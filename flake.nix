d34818bbfa8b38f657385dfede2afd1e387e735f
Daniel Barter <danielbarter@gmail.com>
2021-12-03 15:27:00 -0800

    setting up dev env

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Multi node reaction network generator";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    RNMC.url = github:BlauGroup/RNMC;
+  };
+
+  outputs = { self, nixpkgs, RNMC }:
+    let
+      genericDevShell = systemString:
+        with import nixpkgs { system = systemString; };
+        mkShell {
+          buildInputs = [
+            (python38.withPackages (ps: [ ]))
+          ];
+        };
+
+    in {
+      devShell = {
+        x86_64-linux = genericDevShell "x86_64-linux";
+        x86_64-darwin = genericDevShell "x86_64-darwin";
+      };
+    };
+
+}
