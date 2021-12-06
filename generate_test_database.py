726c16fd7f0925b0f5c5cda6a76ea5c51b69cede
Daniel Barter <danielbarter@gmail.com>
2021-12-05 17:14:11 -0800

    finished generating test database

--- generate_test_database.py
+++ generate_test_database.py
@@ -41,10 +41,15 @@
 
 # for two site interactions, we include both directions in the database
 # two_site_interactions
-# species_id_1|species_id_2|left_state_1|left_state_2|right_state_1|right_state_2|rate
+# species_id_1|species_id_2|
+# left_state_1|left_state_2|
+# right_state_1|right_state_2|rate
 
 # metadata
-# number_of_species|number_of_sites|single_site_interaction_factor|double_site_interaction_factor|spatial_decay_radius
+# number_of_species|number_of_sites|
+# number_of_one_site_interactions|number_of_two_site_interactions|
+# single_site_interaction_factor|double_site_interaction_factor|
+# spatial_decay_radius
 
 
 # test model
@@ -207,7 +212,7 @@ insert_one_site_interaction_sql = """
 
 create_two_site_interactions_table_sql = """
     CREATE TABLE two_site_interactions (
-        species_id_1        INTEGER NOT NULL PRIMARY KEY,
+        species_id_1        INTEGER NOT NULL,
         species_id_2        INTEGER NOT NULL,
         left_state_1        INTEGER NOT NULL,
         left_state_2        INTEGER NOT NULL,
@@ -223,16 +228,18 @@ insert_two_site_interaction_sql = """
 
 create_metadata_table_sql = """
     CREATE TABLE metadata (
-        number_of_species                INTEGER NOT NULL,
-        number_of_sites                  INTEGER NOT NULL,
-        single_site_interaction_factor   REAL NOT NULL,
-        double_site_interaction_factor   REAL NOT NULL,
-        spatial_decay_radius             REAL NOT NULL
+        number_of_species                   INTEGER NOT NULL,
+        number_of_sites                     INTEGER NOT NULL,
+        number_of_one_site_interactions     INTEGER NOT NULL,
+        number_of_two_site_interactions     INTEGER NOT NULL,
+        single_site_interaction_factor      REAL NOT NULL,
+        double_site_interaction_factor      REAL NOT NULL,
+        spatial_decay_radius                REAL NOT NULL
     );
 """
 
 insert_metadata_sql = """
-    INSERT INTO metadata VALUES (?,?,?,?,?);
+    INSERT INTO metadata VALUES (?,?,?,?,?,?,?);
 """
 
 os.system('rm -rf ./scratch; mkdir scratch')
@@ -253,3 +260,57 @@ for s in species:
                   len(species[s]['index_to_state'])))
 
 con.commit()
+
+# insert sites
+for site_id in sites['index_to_site']:
+    site_data = sites['index_to_site'][site_id]
+    cur.execute(insert_site_sql,
+                ( site_id,
+                  site_data[0],
+                  site_data[1],
+                  site_data[2],
+                  species[site_data[3]]['species_id']))
+
+con.commit()
+
+number_of_one_site_interactions = 0
+# inserting single site interactions
+for s in one_site_interactions:
+    for (l,r) in one_site_interactions[s]:
+        number_of_one_site_interactions += 1
+        rate = one_site_interactions[s][(l,r)]
+        cur.execute(insert_one_site_interaction_sql,
+                    ( species[s]['species_id'],
+                      species[s]['state_to_index'][l],
+                      species[s]['state_to_index'][r],
+                      rate))
+
+con.commit()
+
+number_of_two_site_interactions = 0
+# inserting two site interactions
+for (s1,s2) in two_site_interactions:
+    for (l1,l2,r1,r2) in two_site_interactions[(s1,s2)]:
+        number_of_two_site_interactions += 1
+        rate = two_site_interactions[(s1,s2)][
+            (l1,l2,r1,r2)]
+        cur.execute(insert_two_site_interaction_sql,
+                    ( species[s1]['species_id'],
+                      species[s2]['species_id'],
+                      species[s1]['state_to_index'][l1],
+                      species[s2]['state_to_index'][l2],
+                      species[s1]['state_to_index'][r1],
+                      species[s2]['state_to_index'][r2],
+                      rate))
+
+con.commit()
+
+# insert metadata
+cur.execute(insert_metadata_sql,
+            ( len(species),
+              len(sites['index_to_site']),
+              number_of_one_site_interactions,
+              number_of_two_site_interactions,
+              1.0,
+              1.0,
+              4.0))
