e243fdf78c0bb83d7904e72bd3ce18a4e6320593
Daniel Barter <danielbarter@gmail.com>
2022-02-08 15:06:16 -0800

    install sqlite in dev shell

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           buildInputs = [
             (python38.withPackages (ps: [ ]))
             sqlite
+            (builtins.getAttr systemString RNMC.defaultPackage)
           ];
         };
 
