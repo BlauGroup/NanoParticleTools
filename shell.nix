da72c8d4269d9dab2dc7946e7fc98e70c9eb3475
Unknown <sivonxay@gmail.com>
2022-03-14 12:20:08 -0700

    Get Jupyterlab working with nix shell

--- shell.nix
+++ shell.nix
@@ -5,19 +5,35 @@ let
     rev = "45f9a774e981d3a3fb6a1e1269e33b4624f9740e";
   }) {};
 
+  nixpkgs = import (builtins.fetchGit {
+    url = github:NixOS/nixpkgs/nixos-21.05;
+  }) {};
+
+  NanoParticleTools = systemString:
+    with import nixpkgs;
+    with python38Packages;
+    buildPythonPackage {
+      pname = "NanoParticleTools";
+      version = "0.1";
+      src = ./.;
+      checkInputs = [
+        pymatgen
+        monty
+        (builtins.getAttr systemString RNMC.defaultPackage)
+        sqlite
+      ];
+
+    };
+
+
   iPython = jupyter.kernels.iPythonWith {
     name = "python";
-    packages = p: with p; [ numpy ];
-  };
-
-  iHaskell = jupyter.kernels.iHaskellWith {
-    name = "haskell";
-    packages = p: with p; [ hvega formatting ];
+    packages = p: with p; [ numpy NanoParticleTools];
   };
 
   jupyterEnvironment =
     jupyter.jupyterlabWith {
-      kernels = [ iPython iHaskell ];
+      kernels = [ iPython ];
     };
 in
   jupyterEnvironment.env
