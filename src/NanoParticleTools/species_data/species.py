1ac2e96da273f4637b89a9309c6a83c81d443b28
Unknown <sivonxay@gmail.com>
2022-04-06 12:03:11 -0700

    Remove useless lines of code

--- src/NanoParticleTools/species_data/species.py
+++ src/NanoParticleTools/species_data/species.py
@@ -1,5 +1,4 @@
 from monty.json import MSONable
-from pymatgen.core.periodic_table import Species
 import json
 from pathlib import Path
 import os
@@ -7,12 +6,6 @@ from typing import Optional, List
 import numpy as np
 from functools import lru_cache
 
-energies = {'Dy': {
-    'energy_levels': [175, 3626, 5952, 7806, 7853, 9166, 9223, 10273, 11070, 12471, 13267, 13814, 21228, 22222, 23563,
-                      25109, 25794, 25856, 25890, 26334, 27624, 27543],
-    'energy_level_labels': ["6H15/2", "6H13/2", "6H11/2", "6F11/2", "6H9/2", "6F9/2", "6H7/2", "6H5/2", "6F7/2",
-                            "6F5/2", "6F3/2", "6F1/2", "4F9/2", "4H15/2", "4G11/2", "4M21/2", "4F7/2", "4K17/2",
-                            "4I13/2", "6P5/2", "4M19/2", "6P3/2"]}}
 
 SPECIES_DATA_PATH = os.path.join(str(Path(__file__).absolute().parent), 'data')
 
