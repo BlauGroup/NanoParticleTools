c6711e2a82686eb1414ed49f1aec91afbd954dc5
Eric Sivonxay <esivonxay@lbl.gov>
2022-08-02 11:21:55 -0700

    Change learning rate decay milestone=1

--- src/NanoParticleTools/machine_learning/util/learning_rate.py
+++ src/NanoParticleTools/machine_learning/util/learning_rate.py
@@ -3,4 +3,4 @@ from torch.optim.lr_scheduler import LinearLR, ExponentialLR, SequentialLR
 def get_sequential(optimizer):
     linear_lr = LinearLR(optimizer, start_factor=0.01, end_factor=1, total_iters=10)
     exponential_lr = ExponentialLR(optimizer, gamma=0.995)
-    return SequentialLR(optimizer, [linear_lr, exponential_lr], milestones=[1000])
+    return SequentialLR(optimizer, [linear_lr, exponential_lr], milestones=[600])
