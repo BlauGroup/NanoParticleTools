0e781c6dd0ab10eef9d84cca9e6a2d6d170923b4
Eric Sivonxay <esivonxay@lbl.gov>
2023-03-24 12:50:26 -0700

    Add visualization animation

--- src/NanoParticleTools/util/visualization.py
+++ src/NanoParticleTools/util/visualization.py
@@ -1,35 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import animation
+from functools import partial
+from typing import List, Tuple
+from NanoParticleTools.inputs.nanoparticle import NanoParticleConstraint
 
-def plot_nanoparticle(constraints, dopant_specifications, dpi = 150, as_np_array=False):
+
+def plot_nanoparticle(constraints,
+                      dopant_specifications,
+                      dpi=150,
+                      as_np_array=False):
     els = ['Y', 'Yb', 'Er', 'Nd']
 
     n_layers = len(constraints)
     radii = [constraint.radius for constraint in constraints]
-    dopant_dict = [{key:0 for key in els} for _ in range(n_layers)]
+    dopant_dict = [{key: 0 for key in els} for _ in range(n_layers)]
     for dopant in dopant_specifications:
         dopant_dict[dopant[0]][dopant[2]] = dopant[1]
 
     # Fill in the rest with 'Y'
     for layer in dopant_dict:
-        layer['Y'] = 1-sum(layer.values())
+        layer['Y'] = 1 - sum(layer.values())
 
-    vals = [[layer[el] for el in els]for layer in dopant_dict]
+    vals = [[layer[el] for el in els] for layer in dopant_dict]
     cmap = plt.colormaps["tab10"]
-    colors = cmap(np.arange(4)*4)
+    colors = cmap(np.arange(4) * 4)
     colors[0] = [1, 1, 1, 1]
 
     fig = plt.figure(dpi=dpi)
     ax = fig.subplots()
-    for i in list(range(n_layers-1, -1, -1)):
-        ax.pie(vals[i], 
-               radius = radii[i]/radii[-1], 
-               colors= colors, 
+    for i in list(range(n_layers - 1, -1, -1)):
+        ax.pie(vals[i],
+               radius=radii[i] / radii[-1],
+               colors=colors,
                wedgeprops=dict(edgecolor='k'),
-               startangle=90
-              )
-    ax.legend(els, loc='upper left', bbox_to_anchor=(1, 1)
-    )
+               startangle=90)
+    ax.legend(els, loc='upper left', bbox_to_anchor=(1, 1))
     if as_np_array:
         # If we haven't already shown or saved the plot, then we need to
         # draw the figure first.
@@ -37,10 +43,54 @@ def plot_nanoparticle(constraints, dopant_specifications, dpi = 150, as_np_array
 
         # Now we can save it to a numpy array.
         data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3, ))
 
         # Close the figure to remove it from the buffer
         plt.close(fig)
         return data
     else:
         return fig
+
+
+def plot_nanoparticle_on_ax(ax, constraints, dopant_specifications):
+    els = ['Y', 'Yb', 'Er', 'Nd']
+
+    n_layers = len(constraints)
+    radii = [constraint.radius for constraint in constraints]
+    dopant_dict = [{key: 0 for key in els} for _ in range(n_layers)]
+    for dopant in dopant_specifications:
+        dopant_dict[dopant[0]][dopant[2]] = dopant[1]
+    # Fill in the rest with 'Y'
+    for layer in dopant_dict:
+        layer['Y'] = np.round(1 - sum(layer.values()), 3)
+
+    vals = [[layer[el] for el in els] for layer in dopant_dict]
+    cmap = plt.colormaps["tab10"]
+    colors = cmap(np.arange(4) * 4)
+    colors[0] = [1, 1, 1, 1]
+
+    for i in list(range(n_layers - 1, -1, -1)):
+        # print(vals[i])
+        ax.pie(vals[i],
+               radius=radii[i] / radii[-1],
+               colors=colors,
+               wedgeprops=dict(edgecolor='k'),
+               startangle=90)
+    ax.legend(els, loc='upper left', bbox_to_anchor=(1, 1))
+
+
+def update(data, ax):
+    constraints, dopants = data
+    ax.clear()
+    plot_nanoparticle_on_ax(ax, constraints, dopants)
+
+
+def make_animation(frames: List[Tuple[NanoParticleConstraint, Tuple]],
+                   name: str = 'animation.mp4',
+                   fps: int = 30) -> None:
+
+    fig = plt.figure(dpi=150)
+    ax = fig.subplots()
+    anim = animation.FuncAnimation(fig, partial(update, ax=ax), frames=frames)
+    anim.save(name, fps=fps)
+    fig.clear()
