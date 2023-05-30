ba13c06a7cdaecf83b52b50efef3ed9e79e98b34
Eric Sivonxay <esivonxay@lbl.gov>
2023-05-30 16:37:57 -0700

    Remove temporary fireworks files

--- src/NanoParticleTools/flows/fireworks/fireworks.py
+++
@@ -1,9 +0,0 @@
-from fireworks import Firework
-from NanoParticleTools.flows.fireworks.firetasks import NPMCFiretask
-
-
-class NPMCFW(Firework):
-    def __init__(self, **kwargs):
-        tasks = []
-        tasks.append(NPMCFiretask(**kwargs))
-        super(NPMCFW, self).__init__(tasks)
--- src/NanoParticleTools/flows/fireworks/workflows.py
+++
@@ -1,6 +0,0 @@
-from NanoParticleTools.flows.fireworks.fireworks import NPMCFW
-from fireworks import LaunchPad, Workflow, Firework
-
-
-def get_npmc_workflow(**kwargs):
-    return Workflow([NPMCFW(**kwargs)])
