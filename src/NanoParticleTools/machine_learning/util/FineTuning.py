795f5a6e97903768352ca68e85b8266d1fb21107
lattia <lattia@udel.edu>
2023-07-11 13:30:37 -0700

    Updating gitignore

--- src/NanoParticleTools/machine_learning/util/FineTuning.py
+++ src/NanoParticleTools/machine_learning/util/FineTuning.py
@@ -1,5 +1,7 @@
 from NanoParticleTools.machine_learning.models.mlp_model.model import MLPSpectrumModel
 import pytorch_lightning as pl
+from maggma.stores import MongoStore
+
 
 def FreezeMLP(model: MLPSpectrumModel,
                num_frozen_layers: int
@@ -14,4 +16,27 @@ def FreezeMLP(model: MLPSpectrumModel,
         iter_num+=1
         if iter_num < num_frozen_layers*2:
             param.requires_grad = False
+        else:
+            if reset_thawed_layers == True:
+                param.reset
     return model
+
+
+def HF_dataset(store_config: dict,
+               collection_name: str,
+              sample_size : int):
+    """
+    params
+    store_config: dictionary of configuration for MongoDB
+    collection_name: name of collection in MongoDB
+    sample_size: sample size of high fidelity data you want to use in your fine tuning
+    
+    """
+    training_data_store_HF = MongoStore(**store_config, collection_name= collection_name) #access collection
+    training_data_store_HF.connect() #connect to enable aggregate
+    cursor = training_data_store_HF.cursor.allowDiskUse(True)
+    cursor = cursor._collection.aggregate([ {"$sample": { "size": sample_size } } ])
+    training_data_store_HF.count()
+    hf_data = list(cursor)
+
+    return hf_data
