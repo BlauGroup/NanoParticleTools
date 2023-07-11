4a0ead4ed5b124dde901b4e78acba1a8cec2c9e4
lattia <lattia@udel.edu>
2023-07-11 13:31:29 -0700

    Removing HF_dataset func
    depricated function

--- src/NanoParticleTools/machine_learning/util/FineTuning.py
+++ src/NanoParticleTools/machine_learning/util/FineTuning.py
@@ -4,7 +4,8 @@ from maggma.stores import MongoStore
 
 
 def FreezeMLP(model: MLPSpectrumModel,
-               num_frozen_layers: int
+               num_frozen_layers: int,
+               reset_thawed_layers: bool
                ):
     """
     params
@@ -18,25 +19,6 @@ def FreezeMLP(model: MLPSpectrumModel,
             param.requires_grad = False
         else:
             if reset_thawed_layers == True:
-                param.reset
+                param = 0
     return model
 
-
-def HF_dataset(store_config: dict,
-               collection_name: str,
-              sample_size : int):
-    """
-    params
-    store_config: dictionary of configuration for MongoDB
-    collection_name: name of collection in MongoDB
-    sample_size: sample size of high fidelity data you want to use in your fine tuning
-    
-    """
-    training_data_store_HF = MongoStore(**store_config, collection_name= collection_name) #access collection
-    training_data_store_HF.connect() #connect to enable aggregate
-    cursor = training_data_store_HF.cursor.allowDiskUse(True)
-    cursor = cursor._collection.aggregate([ {"$sample": { "size": sample_size } } ])
-    training_data_store_HF.count()
-    hf_data = list(cursor)
-
-    return hf_data
