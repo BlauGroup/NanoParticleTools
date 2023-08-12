dc892783c4585aa0fcf63da9ee94365d3815f26a
Eric Sivonxay <esivonxay@lbl.gov>
2023-08-11 23:50:27 -0700

    Add number of possible dopants as a arugment to the model

--- src/NanoParticleTools/machine_learning/models/hetero/intra_inter_model.py
+++ src/NanoParticleTools/machine_learning/models/hetero/intra_inter_model.py
@@ -21,6 +21,7 @@ class HeteroDCVRepresentationModule(torch.nn.Module):
                  interaction_embedding: bool = True,
                  conc_eps: float = 0.01,
                  geometry_film_layers: List[int] = [16, 16],
+                 n_dopants: int = 3,
                  **kwargs):
         """
         Args:
@@ -37,26 +38,23 @@ class HeteroDCVRepresentationModule(torch.nn.Module):
         self.conc_eps = conc_eps
         self.interaction_embedding = interaction_embedding
 
-        n_conc = 2 if self.use_inverse_concentration else 1
+        self.dopant_embedder = nn.Embedding(n_dopants, embed_dim)
         self.dopant_film_layer = FiLMLayer(1, embed_dim, [16, 16])
 
-        self.dopant_constraint_film_layer = FiLMLayer(
-            2, embed_dim, [16, 16])
+        self.dopant_constraint_film_layer = FiLMLayer(2, embed_dim, [16, 16])
         self.dopant_norm = nn.BatchNorm1d(embed_dim)
 
         if self.interaction_embedding:
-            self.interaction_embedder = nn.Embedding(9, embed_dim)
-            self.intraaction_embedder = nn.Embedding(9, embed_dim)
+            self.interaction_embedder = nn.Embedding(n_dopants**2, embed_dim)
+            self.intraaction_embedder = nn.Embedding(n_dopants**2, embed_dim)
         else:
             self.interaction_embedder = nn.Linear(2, embed_dim)
             self.intraaction_embedder = nn.Linear(2, embed_dim)
         self.integrated_interaction = InteractionBlock(nsigma=nsigma)
         self.interaction_norm = nn.BatchNorm1d(nsigma)
         self.intraaction_norm = nn.BatchNorm1d(nsigma)
-        self.interaction_film_layer = FiLMLayer(nsigma, embed_dim,
-                                                [16, 16])
-        self.intraaction_film_layer = FiLMLayer(nsigma, embed_dim,
-                                                [16, 16])
+        self.interaction_film_layer = FiLMLayer(nsigma, embed_dim, [16, 16])
+        self.intraaction_film_layer = FiLMLayer(nsigma, embed_dim, [16, 16])
 
         self.convs = nn.ModuleList()
         for _ in range(n_message_passing):
@@ -146,7 +144,7 @@ class HeteroDCVRepresentationModule(torch.nn.Module):
         # Multiply the concentration into the integrated_interaction
         interaction_node_conc = dopant_concs[interaction_dopant_indices]
         conc_factor = (interaction_node_conc[:, 0] *
-                        interaction_node_conc[:, 1]).unsqueeze(-1)
+                       interaction_node_conc[:, 1]).unsqueeze(-1)
         integrated_interaction = conc_factor * integrated_interaction
 
         # Normalize the integrated interaction
@@ -161,8 +159,7 @@ class HeteroDCVRepresentationModule(torch.nn.Module):
         # Repeat the same process for the intraaction
         # Embed the interaction nodes, using the pair of dopant types
         if self.interaction_embedding:
-            intraaction_attr = self.intraaction_embedder(
-                intraaction_types)
+            intraaction_attr = self.intraaction_embedder(intraaction_types)
         else:
             intraaction_attr = self.intraaction_embedder(
                 intraaction_type_indices.float())
@@ -176,7 +173,7 @@ class HeteroDCVRepresentationModule(torch.nn.Module):
         # Multiply the concentration into the integrated_intraaction
         intraaction_node_conc = dopant_concs[intraaction_dopant_indices]
         conc_factor = (intraaction_node_conc[:, 0] *
-                        intraaction_node_conc[:, 1]).unsqueeze(-1)
+                       intraaction_node_conc[:, 1]).unsqueeze(-1)
         integrated_intraaction = conc_factor * integrated_intraaction
 
         # Normalize the integrated intraaction
@@ -208,11 +205,36 @@ class HeteroDCVRepresentationModule(torch.nn.Module):
 class HeteroDCVModel(SpectrumModelBase):
 
     def __init__(self,
+                 n_dopants: int = 3,
                  embed_dim: int = 16,
                  n_message_passing: int = 3,
                  nsigma=5,
                  readout_layers: List[int] = [128],
                  **kwargs):
+        """
+        Args:
+            n_dopants: The number of dopants in the system.
+            embed_dim: The dimension of the embedding/latent space.
+            n_message_passing: The number of heterogeneous message passing steps.
+            nsigma: The number of std deviations to use in the gaussian of the integrated
+                interaction. Also corresponds to the number of channels output by the
+                integrated interaction.
+            readout_layers: The number of layers in the readout MLP.
+
+        Inherited Args:
+            l2_regularization_weight: The weight of the L2 regularization term in the loss function.
+                This is passed to the torch optimizer
+            optimizer_type: The type of optimizer to use. options are 'sgd', 'adam', and 'amsgrad'.
+                if 'amsgrad' is selected, the pytorch adam optimizer is used with the `amsgrad=True`
+            learning_rate: The default learning rate for model training. The actual learning rate
+                used may be different depending on the actions of the learning rate scheduler
+            lr_scheduler: The learning rate scheduler class to use.
+            lr_scheduler_kwargs: The kwargs passed to the learning rate scheduler on initialization.
+            loss_function: The loss function to use for backpropagation in training.
+                MAE, MSE, and Cosine Similarity will be logged anyways.
+            additional_metadata: Additional metadata which will be logged with the model to
+                wandb.
+        """
         if 'n_input_nodes' in kwargs:
             warnings.warn(
                 'Cannot override n_input_nodes for this model. It is inferred from'
@@ -227,9 +249,14 @@ class HeteroDCVModel(SpectrumModelBase):
         self.readout_layers = readout_layers
 
         self.representation_module = HeteroDCVRepresentationModule(
-            self.embed_dim, self.n_message_passing, self.nsigma, **kwargs)
+            self.embed_dim,
+            self.n_message_passing,
+            self.nsigma,
+            n_dopants=n_dopants,
+            **kwargs)
 
-        self.readout = NonLinearMLP(embed_dim, 1, self.readout_layers, 0.25, nn.SiLU)
+        self.readout = NonLinearMLP(embed_dim, 1, self.readout_layers, 0.25,
+                                    nn.SiLU)
 
         self.save_hyperparameters()
 
@@ -258,19 +285,21 @@ class HeteroDCVModel(SpectrumModelBase):
 
     def get_inputs(self, data: HeteroData) -> Dict:
 
-        input_dict = {'dopant_types': data['dopant'].types,
-                      'dopant_concs': data['dopant'].x,
-                      'dopant_constraint_indices': data['dopant'].constraint_indices,
-                      'interaction_type_indices': data['interaction'].type_indices,
-                      'interaction_types': data['interaction'].types,
-                      'interaction_dopant_indices': data['interaction'].dopant_indices,
-                      'intraaction_type_indices': data['intraaction'].type_indices,
-                      'intraaction_types': data['intraaction'].types,
-                      'intraaction_dopant_indices': data['intraaction'].dopant_indices,
-                      'edge_index_dict': data.edge_index_dict,
-                      'radii': data.radii,
-                      'constraint_radii_idx': data.constraint_radii_idx,
-                      'batch_dict': data.batch_dict}
+        input_dict = {
+            'dopant_types': data['dopant'].types,
+            'dopant_concs': data['dopant'].x,
+            'dopant_constraint_indices': data['dopant'].constraint_indices,
+            'interaction_type_indices': data['interaction'].type_indices,
+            'interaction_types': data['interaction'].types,
+            'interaction_dopant_indices': data['interaction'].dopant_indices,
+            'intraaction_type_indices': data['intraaction'].type_indices,
+            'intraaction_types': data['intraaction'].types,
+            'intraaction_dopant_indices': data['intraaction'].dopant_indices,
+            'edge_index_dict': data.edge_index_dict,
+            'radii': data.radii,
+            'constraint_radii_idx': data.constraint_radii_idx,
+            'batch_dict': data.batch_dict
+        }
         return input_dict
 
     def get_representation(self, data):
