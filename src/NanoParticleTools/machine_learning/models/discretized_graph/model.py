1c4e73568fba4541bd7a723778a33e4979e2bdce
Eric Sivonxay <esivonxay@lbl.gov>
2023-08-16 12:51:24 -0700

    Refactor Discretized graph to its own files

--- src/NanoParticleTools/machine_learning/models/discretized_graph/model.py
+++ src/NanoParticleTools/machine_learning/models/discretized_graph/model.py
@@ -4,82 +4,10 @@ from torch.nn import functional as F
 from typing import Callable, Union, Optional, List
 import pytorch_lightning as pl
 from torch_geometric import nn as pyg_nn
+from torch_geometric.data import Batch, Data, HeteroData
 from torch_scatter.scatter import scatter
 from NanoParticleTools.machine_learning.core import SpectrumModelBase
-
-
-class CNNModel(SpectrumModelBase):
-
-    def __init__(self,
-                 n_output_nodes=600,
-                 dropout_probability: float = 0,
-                 dimension: Optional[int] = 2,
-                 activation_module: Optional[torch.nn.Module] = nn.ReLU,
-                 conv_params: Optional[List[List]] = None,
-                 nn_layers: Optional[List[int]] = [128],
-                 **kwargs):
-
-        assert dimension > 0 and dimension <= 3
-        if dimension == 1:
-            conv_module = nn.Conv1d
-            pool_module = nn.MaxPool1d
-        elif dimension == 2:
-            conv_module = nn.Conv2d
-            pool_module = nn.MaxPool2d
-        elif dimension == 3:
-            conv_module = nn.Conv3d
-            pool_module = nn.MaxPool3d
-
-        if conv_params is None:
-            conv_params = [(128, 19, 4), (32, 11, 4), (64, 5, 2), (64, 3, 1),
-                           (64, 3, 1)]
-
-        super().__init__(**kwargs)
-
-        self.img_dimension = dimension
-        self.dropout_probability = dropout_probability
-        self.activation_module = activation_module
-        self.n_conv = len(conv_params)
-        self.channels = [layer[0] for layer in conv_params]
-        self.kernel_sizes = [layer[1] for layer in conv_params]
-        self.strides = [layer[2] for layer in conv_params]
-        self.n_output_nodes = n_output_nodes
-
-        # Build the CNN
-        modules = []
-        in_channels = 3  # There are 3 dopants in our initial image
-        for _conv_params in conv_params:
-            modules.append(
-                conv_module(in_channels=in_channels,
-                            out_channels=_conv_params[0],
-                            kernel_size=_conv_params[1],
-                            padding=int((_conv_params[1] - 1) / 2),
-                            stride=_conv_params[2]))
-            modules.append(activation_module())
-            modules.append(pool_module(kernel_size=3, padding=1, stride=2))
-            in_channels = _conv_params[0]
-        # Flatten before putting the outputinto the FCNN
-        modules.append(nn.Flatten())
-
-        for i, n_nodes in enumerate(nn_layers):
-            if self.dropout_probability > 0:
-                modules.append(nn.Dropout(dropout_probability))
-            modules.append(nn.LazyLinear(n_nodes))
-            modules.append(activation_module())
-
-        if self.dropout_probability > 0:
-            modules.append(nn.Dropout(dropout_probability))
-        modules.append(nn.LazyLinear(n_output_nodes))
-        modules.append(nn.ReLU())
-
-        self.nn = nn.Sequential(*modules)
-
-        self.save_hyperparameters()
-
-    def forward(self, x, **kwargs):
-        x = self.nn(x)
-
-        return x
+from NanoParticleTools.machine_learning.modules.core import LazyNonLinearMLP
 
 
 class DiscreteGraphModel(SpectrumModelBase):
