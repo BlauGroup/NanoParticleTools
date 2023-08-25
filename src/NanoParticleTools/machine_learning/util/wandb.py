6296f53f2907eecaeef448bbdcfe1d948f0d0f01
Eric Sivonxay <esivonxay@lbl.gov>
2023-08-25 13:44:55 -0700

    Add function to retrieve many models from wandb by project and tags

--- src/NanoParticleTools/machine_learning/util/wandb.py
+++ src/NanoParticleTools/machine_learning/util/wandb.py
@@ -1,5 +1,6 @@
 import os
 import wandb
+import warnings
 
 
 def download_model(name, run_id, entity, project, overwrite=False):
@@ -15,6 +16,46 @@ def download_model(name, run_id, entity, project, overwrite=False):
 
 
 def model_from_file(model_path, model_cls):
-    model = model_cls.load_from_checkpoint(model_path)
+    model = model_cls.load_from_checkpoint(model_path, strict=False)
     model.eval()
     return model
+
+
+def filter_runs(runs, tags):
+    for run in runs:
+        if all(tag in run.tags for tag in tags):
+            yield run
+
+
+def get_models(entity, project, tags, model_cls, sort_by_name=True):
+    api = wandb.Api()
+    runs = api.runs(path=f"{entity}/{project}")
+
+    runs = list(filter_runs(runs, tags))
+    runs = sorted(runs, key=lambda run: run.name)
+
+    models = []
+    for run in runs:
+        artifacts = run.logged_artifacts()
+
+        # find best artifact
+        best_artifact = None
+        for artifact in artifacts:
+            if 'best' in artifact._aliases:
+                best_artifact = artifact
+                break
+
+        if best_artifact is None:
+            warnings.warn('No best artifact found for run: {}'.format(
+                run.name))
+            continue
+
+        # Download the artifact
+        model_path = best_artifact.download('./checkpoints')
+        model_file = f'{model_path}/{run.name}.ckpt'
+        os.rename(f'{model_path}/model.ckpt', model_file)
+
+        model = model_from_file(model_file, model_cls)
+        models.append(model)
+
+    return models
