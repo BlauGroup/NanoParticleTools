d9647b90289731450e77661ad95a299d7fee7e7d
Eric Sivonxay <esivonxay@lbl.gov>
2023-08-29 18:53:29 -0700

    Update optimization functions

--- src/NanoParticleTools/optimization/scipy_optimize.py
+++ src/NanoParticleTools/optimization/scipy_optimize.py
@@ -1,4 +1,4 @@
-from NanoParticleTools.util.visualization import plot_nanoparticle_from_arrays
+
 from NanoParticleTools.inputs.nanoparticle import SphericalConstraint
 from NanoParticleTools.machine_learning.data import FeatureProcessor
 
@@ -6,7 +6,6 @@ from torch_geometric.data import HeteroData
 from scipy.optimize import Bounds
 from scipy.optimize import LinearConstraint
 
-from matplotlib import pyplot as plt
 import numpy as np
 import torch
 import pytorch_lightning as pl
@@ -14,34 +13,7 @@ import pytorch_lightning as pl
 from collections.abc import Callable
 
 
-def get_plotting_fn(feature_processor: FeatureProcessor) -> Callable:
-    n_elements = len(feature_processor.possible_elements)
-
-    def plotting_fn(x, f=None, accept=None):
-        # Trim #'s less than 0 so they don't cause issues in the plotting
-        x = x.clip(0)
-
-        plt.figure()
-        n_constraints = len(x) // (n_elements + 1)
-        plot_nanoparticle_from_arrays(
-            np.concatenate(([0], x[-n_constraints:])),
-            x[:-n_constraints].reshape(n_constraints, -1),
-            dpi=80,
-            elements=feature_processor.possible_elements,
-        )
-        if f is not None:
-            plt.text(0.1,
-                     0.95,
-                     f'UV Intensity={np.power(10, -f)-100:.2f}',
-                     fontsize=20,
-                     transform=plt.gca().transAxes)
-        plt.show()
-
-    return plotting_fn
-
-
-def get_bounds(n_constraints: int, n_elements: int,
-               r_max: int | float) -> Bounds:
+def get_bounds(n_constraints: int, n_elements: int) -> Bounds:
     r"""
     Get the Bounds which are utilized by scipy minimize.
 
@@ -62,7 +34,7 @@ def get_bounds(n_constraints: int, n_elements: int,
     min_bounds = np.concatenate(
         (np.zeros(num_dopant_nodes), np.zeros(n_constraints)))
     max_bounds = np.concatenate(
-        (np.ones(num_dopant_nodes), r_max * np.ones(n_constraints)))
+        (np.ones(num_dopant_nodes), np.ones(n_constraints)))
     bounds = Bounds(min_bounds, max_bounds)
     return bounds
 
@@ -73,7 +45,8 @@ def get_linear_constraints(
         min_thickness: int | float = 5,
         max_thickness: int | float = 50,
         min_core_size: int | float = 10,
-        max_core_size: int | float = 50) -> LinearConstraint:
+        max_core_size: int | float = 50,
+        max_np_radii: int | float = None) -> LinearConstraint:
     """
     Get the linear constraints which are utilized by scipy minimize.
 
@@ -85,6 +58,9 @@ def get_linear_constraints(
         min_core_size: The minimum core size of the nanoparticle
         max_core_size: The maximum core size of the nanoparticle
     """
+    if max_np_radii is None:
+        max_np_radii = max_core_size + max_thickness * (n_constraints - 1)
+
     num_dopant_nodes = n_constraints * n_elements
     lower_constraint = []
     upper_constraint = []
@@ -101,8 +77,8 @@ def get_linear_constraints(
     _constraint = np.zeros(num_dopant_nodes + n_constraints)
     _constraint[-n_constraints] = 1
     constraint_matrix.append(_constraint)
-    lower_constraint.append(min_core_size)
-    upper_constraint.append(max_core_size)
+    lower_constraint.append(min_core_size / max_np_radii)
+    upper_constraint.append(max_core_size / max_np_radii)
 
     # Constraints on the layer thicknesses
     for i in range(n_constraints - 1):
@@ -112,22 +88,22 @@ def get_linear_constraints(
 
         constraint_matrix.append(_constraint)
 
-        lower_constraint.append(min_thickness)
-        upper_constraint.append(max_thickness)
+        lower_constraint.append(min_thickness / max_np_radii)
+        upper_constraint.append(max_thickness / max_np_radii)
 
     linear_constraint = LinearConstraint(constraint_matrix, lower_constraint,
                                          upper_constraint)
     return linear_constraint
 
 
-def x_to_data(inputs: torch.Tensor,
-              feature_processor: FeatureProcessor) -> HeteroData:
+def x_to_data(inputs: torch.Tensor, feature_processor: FeatureProcessor,
+              max_radii) -> HeteroData:
     n_elements = len(feature_processor.possible_elements)
     n_constraints = len(inputs) // (n_elements + 1)
 
     # unpack the inputs
     x = inputs[:n_elements * n_constraints]
-    r = torch.tensor(inputs[n_elements * n_constraints:],
+    r = torch.tensor(inputs[n_elements * n_constraints:] * max_radii,
                      dtype=torch.float32,
                      requires_grad=True)
 
@@ -143,10 +119,11 @@ def x_to_data(inputs: torch.Tensor,
 
 def get_query_fn(model: pl.LightningModule,
                  feature_processor: FeatureProcessor,
+                 max_np_radii: int | float,
                  return_stats: bool = False) -> Callable:
 
     def model_fn(inputs):
-        data = x_to_data(inputs, feature_processor)
+        data = x_to_data(inputs, feature_processor, max_np_radii)
         if return_stats:
             return model.predict_step(data, return_stats=return_stats)
         else:
@@ -158,11 +135,14 @@ def get_query_fn(model: pl.LightningModule,
     return model_fn
 
 
-def get_jac_fn(model: pl.LightningModule,
-               feature_processor: FeatureProcessor) -> Callable:
+def get_jac_fn(
+    model: pl.LightningModule,
+    feature_processor: FeatureProcessor,
+    max_np_radii: int | float,
+) -> Callable:
 
     def jac_fn(inputs):
-        data = x_to_data(inputs, feature_processor)
+        data = x_to_data(inputs, feature_processor, max_np_radii)
         data['dopant'].x.requires_grad = True
         data['radii_without_zero'].requires_grad = True
 
@@ -171,7 +151,8 @@ def get_jac_fn(model: pl.LightningModule,
         y_hat.backward()
         return np.concatenate(
             (data['dopant'].x.grad.flatten().detach().numpy(),
-             data['radii_without_zero'].grad.flatten().detach().numpy()))
+             data['radii_without_zero'].grad.flatten().detach().numpy() /
+             max_np_radii))
 
     return jac_fn
 
